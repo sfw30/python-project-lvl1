b6955007a440284cf5d01b71c9b52b101867e197
ilya <sfranwar30@gmail.com>
2022-07-30 01:01:45 +0300

    add new game - GCD

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,12 @@
+from math import gcd
+from random import randint
+
+
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+
+def generate_qa_pair():
+	task = '{} {}'.format(randint(0, 100), randint(0, 100))
+	gcd_numbers = task.split(' ')
+	result = str(gcd(int(gcd_numbers[0]), int(gcd_numbers[1])))
+	return (task, result)
