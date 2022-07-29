5fb5aafc60a0eee068f2aaa3d7a14a5b043b1829
ilya <sfranwar30@gmail.com>
2022-07-30 00:08:02 +0300

    add new game - brain calc

---
+++ brain_games/games/calc.py
@@ -0,0 +1,11 @@
+from random import randint, choice
+
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def generate_qa_pair():
+	op = ['-', '+', '*']
+	task = '{} {} {}'.format(randint(0, 100), choice(op) , randint(0, 100))
+	result = str(eval(task))
+	return (task, result)
