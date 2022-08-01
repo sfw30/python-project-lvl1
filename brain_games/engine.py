5c19dac85d4e5f8451baf9db27d035cbe3a23688
ilya <sfranwar30@gmail.com>
2022-08-01 15:15:05 +0300

    change game rules location in engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,9 +3,9 @@ from brain_games import cli
 
 def run(game):
 	cli.welcome()
-	print(game.DESCRIPTION)
 	name = cli.get_name()
 	cli.greet(name)
+	print(game.DESCRIPTION)
 	for _ in range(3):
 		q, a = game.generate_qa_pair()
 		print('Question: {}'.format(q))
