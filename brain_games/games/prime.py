347a30998d043dde7efb76736c15c80ac5dc69d9
ilya <sfranwar30@gmail.com>
2022-08-01 15:33:36 +0300

    fix bugs in math logic

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,14 +1,18 @@
 from random import randint
+from math import sqrt, ceil
 
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-	return (number % 2) == 0
-	
+    for i in range(2, ceil(sqrt(number)) + 1):
+        if (number % i) == 0:
+            return False
+    return True
+
 
 def generate_qa_pair():
 	number = randint(0, 100)
-	prime = 'no' if is_prime(number) else 'yes'
-	return(number, prime)
+	result = 'yes' if is_prime(number) else 'no'
+	return(number, result)
